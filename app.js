05a61078450e1afc4d3c088785cb96fbe2842bdd
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-02-20 22:18:34 -0600

    update with circle-ci

--- app.js
+++ app.js
@@ -16,11 +16,31 @@ var app = express();
 app.use(compression()); //Compress all routes
 app.use(helmet());
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+/*
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'client/build')))
 } else {
   app.use(express.static(path.join(__dirname, 'public')));
 }
+*/
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json()); // support json encoded bodies
